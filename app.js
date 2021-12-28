1798fcec92d0a7017cf358508181ff2daa06ee27
Yeiner Hidalgo <yeinerhidalgomolina@gmail.com>
2021-12-28 16:48:07 -0500

    Construyendo el entorno donde colocar la pregunta con el archivo UI.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //@ts-check
 import { questions } from "./data/questions.js";
 import { Quiz } from "./models/Quiz.js";
+import { UI } from "./models/UI.js";
 
 function main() {
   const quiz = new Quiz(questions);
+  const ui = new UI();
 
+  ui.showQuestion("esta es la pregunta");
 }
 
 main();
--- models/UI.js
+++ models/UI.js
@@ -1,5 +1,14 @@
 export class UI {
-    constructor() {
-        
+    constructor() {}
+
+
+    /**
+     * 
+     * @param {string} text  pregunta para renderisar
+     */
+    showQuestion(text){
+        const questionTitle = document.getElementById("question")
+        console.log(questionTitle)
+
     }
 }
