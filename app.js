f72d53f4e8901e65d81faea41f75d5b44016d0b8
Yeiner Hidalgo <yeinerhidalgomolina@gmail.com>
2021-12-28 11:55:51 -0500

    construyendo estilos(styles) y Clase de objetos(Question) y lista de preguntas(data) funciones de java(app)

---
+++ app.js
@@ -0,0 +1,5 @@
+import { data } from "./data/data.js";
+
+console.log(data);
+
+import "./models/Question.js"
---
+++ models/Question.js
@@ -0,0 +1,15 @@
+class Question {
+  constructor() {
+    this.text="Alguna pregunta";
+    this.choices=["Opciones", 1, 2, 3,];
+    this.answer="Respuesta";
+  }
+}
+
+const question = new Question()
+const question01 = new Question()
+const question02 = new Question()
+const question03 = new Question()
+const question04 = new Question()
+
+console.log(question, question01, question02, question03, question04)
