a7aa2212fc75cf0d2a63031b936e2de35823e222
Yeiner Hidalgo <yeinerhidalgomolina@gmail.com>
2021-12-30 09:36:22 -0500

    30 preguntas agregadas cambio de colores listo para relizar el push a GitHub

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 export const data = [
   {
     question: "¿Quién tiene más probabilidades de trabajar con bases de datos?",
-    choices: ["Back-end developer", "Front-end developer", "incorrrecta2", "correcta3"],
+    choices: ["Back-end developer", "Front-end developer", "Todas las anteriores", "Ninguna es correcta"],
     answer: "Back-end developer",
   },
 
@@ -13,7 +13,7 @@ export const data = [
 
   {
     question: "¿Qué proceso crea y prueba automáticamente su código?",
-    choices: ["CI (Continuous Integration)", "CD (Continuous Delivery)", "incorrrecta2", "correcta3"],
+    choices: ["CI (Continuous Integration)", "CD (Continuous Delivery)", "Todas las anteriores", "Ninguna es correcta"],
     answer: "CI (Continuous Integration)",
   },
 
@@ -25,7 +25,7 @@ export const data = [
 
   {
     question: "¿Qué administrador (es) de paquetes usa Python?",
-    choices: ["pip y conda", "RPM y npm", "incorrrecta2", "correcta3"],
+    choices: ["pip y conda", "RPM y npm", "Todas las anteriores", "Ninguna es correcta"],
     answer: "pip y conda",
   },
 
@@ -37,7 +37,7 @@ export const data = [
 
   {
     question: "¿A qué asociaría una API?",
-    choices: ["Back-end", "Front-end", "incorrrecta2", "correcta3"],
+    choices: ["Back-end", "Front-end", "Todas las anteriores", "Ninguna es correcta"],
     answer: "Back-end",
   },
 
@@ -72,111 +72,111 @@ export const data = [
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cómo se llaman los bloques de construcción de HTML?",
+    choices: ["Chunks", "Nodes", "Elements", "Attributes"],
+    answer: "Elements",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué etiqueta se utiliza para proporcionar palabras clave para los motores de búsqueda?",
+    choices: ["<meta>", "<title>", "<header>", "<footer>"],
+    answer: "<meta>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "Complete el espacio en blanco: en HTML5, ______________ le permite ejecutar tareas de procesamiento intensivo sin bloquear la interfaz de usuario.",
+    choices: ["Structural Elements", "Web workers", "DOM Accessors", "Non-blocking feeds"],
+    answer: "Web workers",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "Al definir un hipervínculo (<a>), ¿qué atributo se utiliza para especificar la dirección de destino?",
+    choices: ["target", "src", "href", "ref"],
+    answer: "href",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cómo se escriben los comentarios en HTML?",
+    choices: ["< !-- Comentario -- >", "/*Comentario*/", "“””Comentario”””", "#Comentario"],
+    answer: "< !-- Comentario -- >",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué etiqueta conserva los espacios y los saltos de línea de un texto determinado cuando se muestra? ",
+    choices: ["<label>", "<pre>", "<p>", "<section>"],
+    answer: "<pre>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué etiqueta  NO SE PUEDE  utilizar como contenedor de texto?",
+    choices: ["<br>", "<cuerpo>", "<p>", "<h1>"],
+    answer: "<br>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cuál es el selector universal en CSS?",
+    choices: ["@", "*", "#", "?"],
+    answer: "*",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué etiqueta se utiliza para crear una lista ordenada?",
+    choices: ["<li>", "<orderedlist>", "<ul>", "<ol>"],
+    answer: "<ol>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué selector ayuda a seleccionar todos los elementos de un documento, excepto los párrafos?",
+    choices: [":except(p)", ":not(p)", ":n(p)", ":!(p)"],
+    answer: ":not(p)",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué tipo de elemento es <article>?",
+    choices: ["Un elemento gráfico", "Un elemento de entrada", "Un elemento semántico", "Un elemento multimedia"],
+    answer: "Un elemento semántico",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cuál de las siguientes etiquetas define un título para un elemento <fieldset>?",
+    choices: ["<legend>", "<title>", "<label>", "<caption>"],
+    answer: "<legend>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cómo se define el tipo de documento en HTML5? ",
+    choices: ["<!DOCTYPE html!>", "<!DOCTYPE html>", "<!DOCTYPE html5>", "<!DOCTYPE “text/html5”>"],
+    answer: "<!DOCTYPE html>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué elemento define un título para el elemento <figure>?",
+    choices: ["<figcaption>", "<title>", "<legend>", "<label>"],
+    answer: "<figcaption>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cuál es la codificación de caracteres predeterminada en HTML5?",
+    choices: ["ISO-8859-1", "UTF-16", "UTF-8", "UTF-32"],
+    answer: "UTF-8",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Cuál de las siguientes es una nueva función de formulario agregada en HTML5?",
+    choices: ["<reset>", "<output> ", "<submit>", "<a>"],
+    answer: "<output>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué etiqueta semántica se utiliza para mostrar cualquier contenido que esté  indirectamente  relacionado con el contenido principal del documento? ",
+    choices: ["<detalles>", "<artículo>", "<sección>", "<aparte>"],
+    answer: "<aparte>",
   },
   
   {
-    question: "¿Cuál de las siguientes NO es una habilidad necesaria para un desarrollador front-end?",
-    choices: ["CSS", "HTML", "JavaScript", "Java"],
-    answer: "Java",
+    question: "¿Qué propiedad se utiliza para establecer el contenido dentro de un elemento HTML?",
+    choices: ["element.content", "element.innerHTML", "element.html", "element.data"],
+    answer: "element.innerHTML",
   },
   
 ];
